3d526f75617581ec03ffc2e587b505d772143d1b
Evan Bonertz <evan.bonertz@commercetools.com>
2016-10-21 12:30:51 -0400

    Add Orders method

--- index.js
+++ index.js
@@ -16,11 +16,32 @@ client.customers.fetch()
       process.exit(1)
     }
   } else {
-    console.log(result)
+    console.log(result, null, 2)
     console.log(JSON.stringify(result))
     console.log("These are all the Australian customers")
-    process.exit(0)
+    // process.exit(0)
   }
+})
+.catch(function (err){
+  console.error(err)
+  process.exit(1)
+})
+
+client.orders.fetch()
+.then( function getOrders(result){
+  if (argv.expects) {
+    if (arg.expects == result.body.count) {
+      console.log('Expectation passed')
+      process.exit(0)
+    } else {
+      console.error('Expected to get some Orders')
+      process.exit(1)
+    }
+    } else {
+      console.log(JSON.stringify(result))
+      console.log("There are no orders yet")
+      process.exit(0)
+    }
 })
 .catch(function (err){
   console.error(err)
