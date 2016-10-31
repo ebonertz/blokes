5c376c6e13b08182819966e77f32acc863a01a34
Evan Bonertz <evan.bonertz@commercetools.com>
2016-10-31 17:04:37 -0400

    Exit after order method

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.customers.fetch()
     console.log(result, null, 2)
     console.log(JSON.stringify(result))
     console.log("These are all the Australian customers")
-    // process.exit(0)
+    process.exit(0)
   }
 })
 .catch(function (err){
