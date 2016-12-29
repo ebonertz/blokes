f96e5b6b7217db8e9761fdb4d4a92e3aa6ce66ce
Evan Bonertz <evan.bonertz@commercetools.com>
2016-12-29 15:37:39 -0500

    Linted the shit out of config, gulpfile and index

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
-const { SphereClient } = require('sphere-node-sdk')
-const Config = require('./config.js')
-const http = require('http')
-const argv = require('minimist')(process.argv.slice(2))
-console.log(`SphereClient: ${SphereClient}`)
-const client = new SphereClient(Config)
+const { SphereClient } = require('sphere-node-sdk');
+const Config = require('./config.js');
+const argv = require('minimist')(process.argv.slice(2));
+
+const client = new SphereClient(Config);
+// console.log(`SphereClient: ${SphereClient}`)
 
 client.customers.fetch()
-.then(function getCustomers(result){
-  if (argv.expects) {
-    if (argv.expects == result.body.count) {
-      console.log('Expectation passed')
-      process.exit(0)
+  .then((result) => {
+    if (argv.expects) {
+      if (argv.expects === result.body.count) {
+        console.log('Expectation passed');
+        process.exit(0);
+      } else {
+        console.error(`Expected ${argv.expects}, found ${result.body.count}`);
+        process.exit(1);
+      }
     } else {
-      console.error('Expected ' + argv.expects + ', found ' + result.body.count)
-      process.exit(1)
+      console.log(result, null, 2);
+      console.log(JSON.stringify(result));
+      console.log('These are all the Australian customers');
     }
-  } else {
-    console.log(result, null, 2)
-    console.log(JSON.stringify(result))
-    console.log("These are all the Australian customers")
-    // process.exit(0)
-  }
-})
-.catch(function (err){
-  console.error(err)
-  process.exit(1)
-})
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
 
 client.orders.fetch()
-.then(function getOrders(result){
-  if (argv.expects) {
-    if (arg.expects == result.body.count) {
-      console.log('Expectation passed')
-      process.exit(0)
+  .then((result) => {
+    if (argv.expects) {
+      if (argv.expects === result.body.count) {
+        console.log('Expectation passed');
+        process.exit(0);
+      } else {
+        console.error('Expected to get some Orders');
+        process.exit(1);
+      }
     } else {
-      console.error('Expected to get some Orders')
-      process.exit(1)
+      console.log(JSON.stringify(result));
+      console.log('There are no orders yet, sorry');
     }
-    } else {
-      console.log(JSON.stringify(result))
-      console.log("There are no orders yet, sorry")
-      // process.exit(0)
-    }
-})
-.catch(function (err){
-  console.error(err)
-  process.exit(1)
-})
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
